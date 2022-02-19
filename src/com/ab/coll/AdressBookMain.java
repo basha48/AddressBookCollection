ed5270e448f1a4eb38c37f7f80558fcf4be2cdd9
basha <bashamb787@gmail.com>
2022-02-19 11:58:51 +0530

    create contact person to address book

--- src/com/ab/coll/AdressBookMain.java
+++ src/com/ab/coll/AdressBookMain.java
@@ -2,6 +2,6 @@ package com.ab.coll;
 
 public class AdressBookMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome to address book");
+		AddressBook addressBook = new AddressBook();
 	}
 }
