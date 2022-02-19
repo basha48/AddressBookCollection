80b9ca962e2f671a72bc446a515255b504d3d922
basha <bashamb787@gmail.com>
2022-02-19 12:45:03 +0530

    edit contact using name

--- src/com/ab/coll/AdressBookMain.java
+++ src/com/ab/coll/AdressBookMain.java
@@ -4,5 +4,6 @@ public class AdressBookMain {
 	public static void main(String[] args) {
 		AddressBook addressBook = new AddressBook();
 		addressBook.addContact();
+		addressBook.editContact();
 	}
 }
